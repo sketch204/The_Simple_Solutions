cbce509504f995608331b3605ce464dddaa120df
Inal Gotov <sketch204@gmail.com>
2017-04-02 14:52:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('*', function(req, res) {
     res.sendFile(__dirname + req.path);
 });
 
-app.listen(3000, function() {
+app.listen('/', function() {
     console.log("Server started on port 3000");
 });
