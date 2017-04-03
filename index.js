3c72490eb66f641c98084824e5a82032d8c34d34
Inal Gotov <sketch204@gmail.com>
2017-04-02 20:18:57 -0400

    Update Port numbers

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let pug = require('pug');
 
 let app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('*', function(req, res) {
     console.log("Requrested Path: " + __dirname + req.path);
     res.sendFile(__dirname + req.path);
 });
 
-app.listen(80, function() {
+app.listen(port, function() {
     console.log("Server started on port 80");
 });
